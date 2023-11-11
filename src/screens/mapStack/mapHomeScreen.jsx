2a81d342a751b91c8eac95309c5b10be7c46c996
3bobchen <83627389+3bobchen@users.noreply.github.com>
2023-11-11 20:08:31 +0100

    Map buttons

--- src/screens/mapStack/mapHomeScreen.jsx
+++ src/screens/mapStack/mapHomeScreen.jsx
@@ -1,9 +1,63 @@
 import {StatusBar} from 'expo-status-bar';
 import {Pressable, StyleSheet, Text, View} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import Icon from 'react-native-vector-icons/FontAwesome'
+import {useEffect, useState} from "react";
+
+const originalLocations = [
+    {
+        lat: -33.872598,
+        long: 151.206317,
+        color: "blue",
+        title: "",
+        description: ""
+    },
+    {
+        lat: -33.871038,
+        long: 151.207602,
+        color: "black",
+        title: "State Theatre",
+        description: ""
+    },
+    {
+        lat: -33.86661,
+        long: 151.207472,
+        color: "black",
+        title: "ivy",
+        description: ""
+    },
+    {
+        lat: -33.873304,
+        long: 151.2083,
+        color: "blue",
+        title: "",
+        description: ""
+    },
+    {
+        lat: -33.869583,
+        long: 151.205152,
+        color: "blue",
+        title: "",
+        description: ""
+    }
+]
 
 const MapHomeScreen = () => {
+    const [showmt, setShowmt] = useState(false)
+    const [filter, setFilter] = useState({p: true, a: true})
+    const [locations, setLocations] = useState(originalLocations)
+    useEffect(() => {
+        const newLocs = []
+        originalLocations.forEach((l) => {
+            if (l.color === "blue" && filter.p) {
+                newLocs.push(l)
+            }
+            if (l.color === "black" && filter.a){
+                newLocs.push(l)
+            }
+        })
+        setLocations(newLocs)
+    }, [filter]);
     return (
         <View style={styles.container}>
             <View style={{
@@ -36,24 +90,50 @@ const MapHomeScreen = () => {
                     backgroundColor: "#DDE2F5",
                     padding: 5,
                     borderRadius: 10
+                }} onPress={() => {
+                    setShowmt(!showmt)
                 }}>
                     <Text>Marker Type</Text>
                     <Icon name="chevron-down" size={15} color="#900"/>
                 </Pressable>
+                {
+                    showmt ? <View style={{
+                        position: "absolute",
+                        top: 38,
+                        right: 83,
+                        width: 100,
+                        backgroundColor: "#DDE2F5",
+                        borderRadius: 5
+                    }}>
+                        <Pressable onPress={() => {setFilter({p: !filter.p, a: filter.a})}} style={{height: 35, justifyContent: "center", padding: 5}}><Text>People {filter.p ?
+                            <Icon name="check-circle-o" size={15} color="#900"/> : null}</Text></Pressable>
+                        <Pressable onPress={() => {setFilter({p: filter.p, a: !filter.a})}}
+                            style={{height: 35, justifyContent: "center", padding: 5}}><Text>Activities {filter.a ?
+                            <Icon name="check-circle-o" size={15} color="#900"/> : null}</Text></Pressable>
+                    </View> : null
+                }
             </View>
             <MapView
                 style={styles.map}
                 camera={{
                     center: {
-                        latitude: -33.87055,
-                        longitude: 151.20596,
+                        latitude: -33.86955,
+                        longitude: 151.20696,
                     },
-                    pitch: 40,
-                    altitude: 1450,
+                    pitch: 0,
+                    altitude: 1850,
                     zoom: 1650
                 }}
-                showsPointsOfInterest={true}
-            />
+                showsPointsOfInterest={false}
+                showsUserLocation={true}
+            >
+                {
+                    locations.map((l)=>{
+                        return <Marker coordinate={{latitude: l.lat, longitude: l.long}} pinColor={l.color} title={l.title}
+                                       description={l.description}/>
+                    })
+                }
+            </MapView>
             <StatusBar style="auto"/>
         </View>
     );
